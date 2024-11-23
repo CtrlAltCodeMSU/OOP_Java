f4ba9a5e322c9853d52ec707f4cd2c8753eb2e28
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2024-11-23 12:24:03 +0500

    Poly

--- University_Tasks_and_practice/src/Polymorphism/motor.java
+++ University_Tasks_and_practice/src/Polymorphism/motor.java
@@ -1,5 +1,19 @@
 package Polymorphism;
+class vehicle{
+	void run(){
+		System.out.println("Vehicle is Running");
+	}
+}
+class Bike extends vehicle{
+	@Override
+	void run(){
+		System.out.println("Bike is Running");
+	}
+}
 
 public class motor {
-
+	public static void main(String[] args) {
+		vehicle myVehicle = new Bike();
+		myVehicle.run();
+	}
 }
