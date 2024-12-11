b7546b74876f8a13cc74dae57575196ab756de0a
CtrlAltCodeMSU <muhammadsanaullah4443@gmail.com>
2024-12-11 21:18:23 +0500

    update

---
+++ Java_Course/src/Exception/Exception.java
@@ -0,0 +1,44 @@
+package Exception;
+
+public class Exception {
+////	public void show() {
+////		System.out.println("This is custom class");
+////	}
+//}
+//	class main{
+//		public static void main(String[] args) {
+////			Exception my = new Exception();
+////			my.show();
+//			int[] arr = new int[4];
+////			int i = arr[4];
+//			System.out.println("I want to execute");
+//		}
+//static void fun()
+//{
+//	try {
+//		throw new NullPointerException("demo");
+//	}
+//	catch (NullPointerException e) {
+//		System.out.println("Caught inside fun().");
+//		throw e; // rethrowing the exception
+//	}
+//}
+//
+//	public static void main(String args[])
+//	{
+//		try {
+//			fun();
+//		}
+//		catch (NullPointerException e) {
+//			System.out.println("Caught in main.");
+//		}
+//	}
+public static void main(String[] args)
+	 throws InterruptedException
+{
+	Thread.sleep(10);
+	System.out.println("Hello Geeks");
+}
+	}
+
+
--- Java_Course/src/Lab_09/RunCal.java
+++ Java_Course/src/Lab_09/RunCal.java
@@ -1,12 +1,24 @@
 package Lab_09;
 
+//public class RunCal {
+//	public static void main(String[] args) {
+//		Calculator my = new Calculator();
+//		System.out.println(my.add(2 ,3));
+//		System.out.println(my.add(2 , 5 , 5 ));
+//		System.out.println(my.add(2.5f ,3));
+//		System.out.println(my.add(2.5f ,3.6f));
+//		System.out.println(my.add(2 , 3.5f));
+//	}
+//}
 public class RunCal {
 	public static void main(String[] args) {
-		Calculator my = new Calculator();
-		System.out.println(my.add(2 ,3));
-		System.out.println(my.add(2 , 5 , 5 ));
-		System.out.println(my.add(2.5f ,3));
-		System.out.println(my.add(2.5f ,3.6f));
-		System.out.println(my.add(2 , 3.5f));
+		int num1 = 10;
+		int num2 = 0;
+
+		// This will cause an ArithmeticException at runtime
+		System.out.println("Result: " + (num1 / num2));
+
+		System.out.println("This line won't execute.");
 	}
 }
+
